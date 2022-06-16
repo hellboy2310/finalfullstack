6707036de5eaa469321cff19cbea2c06d85e535a
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-06-16 23:38:05 +0530

    changes in todo

---
+++ module4/react/todo-app/src/components/Todo.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+
+class Todo extends Component{
+    render(){
+        return(
+            <div>This is todo component</div>
+        )
+    }
+}
+export default Todo;
