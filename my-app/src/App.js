0a69554e2bfe5e45d02765e7f47952b75e3d610d
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-06-18 23:44:17 +0530

    movie app started

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import Banner from './components/Banner';
+function App() {
+  return (
+    <div>
+    <NavBar/>
+    <Banner/>
+  </div>
+    );
+}
+
+export default App;
