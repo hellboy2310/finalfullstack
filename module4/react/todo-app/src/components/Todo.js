32affa2668be7e4765133a71ea6bb6242f2490e6
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-06-21 21:21:41 +0530

    odified

--- module4/react/todo-app/src/components/Todo.js
+++ module4/react/todo-app/src/components/Todo.js
@@ -2,10 +2,56 @@ import { Component } from "react";
 
 
 class Todo extends Component{
+    constructor(){
+        super();
+        this.state = {
+            tasks:[],
+            currTask:""
+        }
+    }
+    handleChange = (e) =>{
+        this.setState({
+            currTask:e.target.value
+        })
+    }
+    handleAddTask = () =>{
+        this.setState({
+            tasks:[...this.state.tasks,{task:this.state.currTask,id:this.state.tasks.length+1}],
+            currTask:""
+        })
+    }
+    
+    
+    hadleDelete = (id) => {
+        let narr = this.state.tasks.filter((taskObj) =>{
+            return taskObj.id !=id;
+        })
+        this.setState({
+            tasks:[...narr]
+        })
+    }
+
+
+
     render(){
         return(
-            <div>This is todo component</div>
-        )
+            <div>
+                <input type = "text" value = {this.state.currTask} onChange ={this.handleChange}/>
+                <button onClick = {this.handleAddTask}>Add task</button>
+            
+                    <ul>
+                      {/* here loop does not work so we need to loop through map done below */}
+                        {this.state.tasks.map((taskObj)=>(
+                            <li key = {taskObj.id}>
+                                <p>{taskObj.task}</p>
+                                <button onClick={()=>this.hadleDelete(taskObj.id)}>Delete</button>
+                            </li>
+                        ))}
+                    </ul>
+
+            </div>
+
+            )
     }
 }
 export default Todo;
