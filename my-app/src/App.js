d019f65096d8ce9ed6f533c9a8ece1d0619a3de2
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-06-28 23:42:42 +0530

    movies app local storage filter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,21 @@ import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import MovieList from './components/MovieList';
 import Fav from './components/Fav'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 function App() {
   return (
-    <div>
+    
+      <BrowserRouter>
     <NavBar/>
     {/* <Banner/>
     <MovieList/> */}
-    <Fav/>
-  </div>
+    <Routes>
+      <Route path = "/" element = {<><Banner/><MovieList/></>}/>
+      <Route path = "/favourites" element = {<Fav/>}/>
+    </Routes>
+    </BrowserRouter>
+    
+  
     );
 }
 
