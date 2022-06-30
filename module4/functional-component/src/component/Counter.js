52ad737a2d7ddee05d73cffe89e790383b3ff821
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-06-30 23:33:31 +0530

    counter using functional programming

---
+++ module4/functional-component/src/component/Counter.js
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function Counter(){
+    const [count,setCount] = useState(0)
+    const [text,setText] = useState('')
+return (
+    <div>
+        <h1>this is a counter{count}</h1>
+        <button onClick = {() => setCount(count+1)}>+</button>
+        <button onClick = {() => setCount(count-1)}>-</button>
+        <input value={text} onChange = {(e) => setText(e.target.value)}></input>
+        <h1>{text}</h1>
+    </div>
+)
+}
+export default Counter
