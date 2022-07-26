b5a0b0877bee56590eaa424fb9d51e13a0d78ce0
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-07-27 00:15:11 +0530

    redux started

---
+++ module4/shopping-cart/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+let Navbar = () => {
+    return(
+        <>
+          <nav className="navbar navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a className="navbar-brand">Shopping Cart</a>
+            </div>
+            </nav>
+        </>
+    )
+}
+export default Navbar;
