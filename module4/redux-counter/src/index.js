035b12dd1dcb292af43224a6059a84c86c06e586
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-07-30 21:12:02 +0530

    redux-counter done

---
+++ module4/redux-counter/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+import { combineReducers,createStore } from 'redux';
+import {counterReducer, loginReducer} from "./redux/reducer"
+import { Provider } from 'react-redux';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let rootReducer = combineReducers({
+  count:counterReducer,
+  logged:loginReducer,
+})
+
+let myStore = createStore(rootReducer);
+
+
+
+
+
+root.render(
+ <Provider store = {myStore}>
+ <App/>
+ </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
---
+++ module4/redux-counter/src/redux/action.js
@@ -0,0 +1,25 @@
+export const incrementCounter = (value)=>{
+    return {
+        type:"INCREMENT",
+        payload:value
+    }
+}
+
+export const decrementCounter = () =>{
+    return {
+        type:"DECREMENT"
+    }
+}
+
+export const loginCreator = ()=>{
+    return {
+        type:"LOGIN"
+    }
+}
+
+
+export const logoutCreator = () =>{
+    return {
+        type:"LOGOUT"
+    }
+}
