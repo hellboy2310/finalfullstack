1cd11132a23e411387ef764c91d302193a350786
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-08-19 00:46:17 +0530

    foodapp

--- module5/FoodApp/server.js
+++ module5/FoodApp/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const cookieParser = require('cookie-parser');
 
+var jwt = require('jsonwebtoken');
+const secretKey = 'lkanjgioahgiavvdalhga';
+
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -15,12 +19,12 @@ app.listen(3000,function(){
 app.post("/signup",async function(req,res){
     try{
         let data = req.body;
-    
+        
     let newUser = await userModel.create(data);
         console.log(newUser);
     res.json({
         message:"data received",
-        
+        data:data
     })
 }
 catch(err){
@@ -34,12 +38,19 @@ app.post("/login",async function(req,res){
             let{email,password} = data;
             if(email && password){
                 let user = await userModel.findOne({email:email});
-                  
+                      
                   console.log(user);
                 if(user){
                     if(user.password == password){
-                        res.cookie("token","sample value");
+                        
+                        //create jwt -> payload ,secret key , algo by default -> SHA256
+                        const token  = jwt.sign({data:user['_id']},secretKey);
+                        console.log(token);
+
+
+                        res.cookie("JWT",token);
                         res.send("user logged in");
+                        
                     }
                     else{
                         res.send("Email or Password does not match");
@@ -60,6 +71,117 @@ app.post("/login",async function(req,res){
     
 })
 
-app.get("/users",function(req,res){
-    console.log(req.cookies);
+
+app.patch("/forgotPassword",async function(req,res){
+    try{
+        let {email} = req.body;
+        let otp = otpGenerator();
+        let afterFiveMin  = Date.now() + 1000*60*5;
+        let user = await userModel.findOneAndUpdate({email:email},{otp:otp,otpExpiry:afterFiveMin},{new:true});
+        console.log(user);
+        res.json({
+            data:user,
+            "message":"otp sent to your mail"
+        })
+    }
+    catch(err){
+        res.send(err.message);
+    }
 })
+
+app.patch("/resetPassword",async  function(req,res){
+    try{
+            let{otp,password,confirmPassword,email} = req.body;
+            let user = await userModel.findOne({email});
+            let currentTime = Date.now();
+            if(currentTime > user.otpExpiry){
+                delete user.otp;
+                delete user.otpExpiry;
+                await user.save();
+                res.json({
+                    message:"otp expired"
+                })
+            }
+            else{
+              if(user.otp != otp)
+             { 
+                res.json({
+                    message:"Otp does not match"
+                })
+              }
+                else{
+                    user = await userModel.findOneAndUpdate({otp},{password,confirmPassword},{runValidators:true,new:true}); 
+                    delete user.otp,
+                    delete user.otpExpiry
+                    await user.save();
+
+                    res.json({
+                        user:user,
+                        message:"user password reset complete"
+                    })
+                }
+            }
+           
+    }
+    catch(err){
+        res.send(err.message);
+    }
+})
+
+function otpGenerator(){
+    return Math.floor(Math.random() * 1000000);
+}
+
+
+
+
+
+
+
+app.get("/users",protectRoute,async function(req,res){
+    try{
+        let users = await userModel.find();
+        res.json(users);
+    }
+    catch(err){
+        res.send(err.message);
+    }
+})
+
+app.get("/user",protectRoute,async function(req,res){
+    try{
+        const userId = req.userId;
+        const user = await userModel.findById(userId);
+        //to send json data
+        res.json({
+            data:user,
+            message:"data about logged in user is send",
+           
+        })
+        
+    }
+    catch(err){
+            res.send(err.message);
+    }
+})
+
+function protectRoute(req,res,next){
+    try{
+        let cookies = req.cookies;
+        let JWT = cookies.JWT;
+        if(cookies.JWT){
+            const token = jwt.verify(JWT,secretKey);
+            console.log(token);
+           let userId = token.data;
+           req.userId = userId;
+            next();
+        }
+        else{
+            res.send("you are not logged in , kindly login");
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.send(err.message);
+    }
+}
